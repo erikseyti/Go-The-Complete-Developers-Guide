9b47c5d4fc5c4cc106ae4ce9da1c45fc36c2e711
Erik Seyti <erikseyti@gmail.com>
2021-09-15 15:09:27 -0300

    Embedded Structs

--- structs/main.go
+++ structs/main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
@@ -16,14 +22,28 @@ func main() {
 	// fmt.Println(alex)
 
 	// Third Way to create a struct person
-	var alex person
+	// var alex person
 
-	fmt.Println(alex)
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+	// fmt.Println(alex)
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+	// alex.contact.email = "alex@hotmail.net"
+	// alex.contact.zipCode = 5111
 
 	// Print exibe todos os valores da struct Person
-	fmt.Printf("%+v", alex)
+	// fmt.Printf("%+v", alex)
+	// fmt.Println("")
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "Jim@gamil.com",
+			zipCode: 94000,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
 	fmt.Println("")
 
 }
