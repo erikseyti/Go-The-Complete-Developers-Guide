2ed2c7326bbc8aa614acbeed21307521a2984b4a
Erik Seyti <erikseyti@gmail.com>
2021-09-27 19:59:29 -0300

    Example of 2 other ways to declares maps in Go!

--- map/main.go
+++ map/main.go
@@ -1,13 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+
+	// var colors map[string]string
+
+	// colors := make(map[string]string)
+	// fruits := make(map[int]string)
 
 	colors := map[string]string{
 		"red": "#ff0000",
 		"green": "#4bf745",
 	}
+
+	// fruits[1] = "Melon"
+	// fmt.Println(fruits)
+	
+	colors["white"] = "#fffff"
+
 	fmt.Println(colors)
-	fmt.Println(colors["red"])
+	fmt.Println(colors["white"])
+
+	// delete(colors, "white")
+	// fmt.Println(colors)
+	
 }
