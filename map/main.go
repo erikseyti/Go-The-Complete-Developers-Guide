1c063c2d5ba38b6735e3e43d3bb3eee7e25cea15
Erik Seyti <erikseyti@gmail.com>
2021-11-19 13:10:03 -0300

    Iterate over a map on Go

--- map/main.go
+++ map/main.go
@@ -14,6 +14,7 @@ func main() {
 	colors := map[string]string{
 		"red": "#ff0000",
 		"green": "#4bf745",
+		"white": "ffffff",
 	}
 
 	// fruits[1] = "Melon"
@@ -21,10 +22,22 @@ func main() {
 	
 	colors["white"] = "#fffff"
 
-	fmt.Println(colors)
-	fmt.Println(colors["white"])
+	// fmt.Println(colors)
+	// fmt.Println(colors["white"])
+
+	printMap(colors)
 
 	// delete(colors, "white")
 	// fmt.Println(colors)
 	
+}
+
+func printMap(c map[string]string) {
+
+	for color, hex := range c {
+		// fmt.Println(color)
+		// fmt.Println(hex)
+		fmt.Println("Hex code for ", color , "is", hex)
+	}
+
 }
