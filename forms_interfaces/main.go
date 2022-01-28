6556ba3abe4ec3bbd357951f24fce2a0de564f4d
Erik Seyti <erikseyti@gmail.com>
2022-01-28 16:41:22 -0300

    Done assignment 2 to create a shape interface that are used to a square and triangle structs

---
+++ forms_interfaces/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type square struct{
+	sideLength float64
+}
+type triangle struct{
+	height float64
+	base float64
+}
+
+
+func main() {
+	sq := square{10}
+	tr := triangle{height: 10, base: 10}
+
+	// fmt.Println(sq.getArea())
+	// fmt.Println(tr.getArea())
+
+	printArea(tr)
+	printArea(sq)
+}
+
+
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return t.base * t.height * 0.5
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
