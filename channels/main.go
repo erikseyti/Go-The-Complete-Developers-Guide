2f24a19633cbf3ca843978635ffa35c93543e15c
Erik Seyti <erikseyti@gmail.com>
2022-06-07 22:19:43 -0300

    example of how to use a function literal (labda function in Python) to call a go routine function call

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -27,7 +28,11 @@ func main() {
 
 	// alternative way to loop a infinite a channel
 	for l := range c {
-		go checkLink(l, c)
+		
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
 	}
 
 }
